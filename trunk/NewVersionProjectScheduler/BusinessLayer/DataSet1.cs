1ce112ca9f1625fd72c27d91086f10fc9315ac7f
greggdavis@gmail.com <greggdavis@gmail.com@c6565826-222e-0410-9169-f7932b4922d6>
2010-08-15 09:33:53 +0000

    Initial add

---
+++ trunk/NewVersionProjectScheduler/BusinessLayer/DataSet1.cs
@@ -0,0 +1,13 @@
+﻿namespace Scheduler.BusinessLayer {
+    
+    
+    public partial class DataSet1 {
+        partial class GetPayrollByInstructorDataTable
+        {
+        }
+    
+        partial class viewPayrollByInstructorDataTable
+        {
+        }
+    }
+}
