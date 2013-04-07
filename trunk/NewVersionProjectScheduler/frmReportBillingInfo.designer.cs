7e3a676a5514679d84073db408ba3905a2359612
greggdavis@gmail.com <greggdavis@gmail.com@c6565826-222e-0410-9169-f7932b4922d6>
2013-04-07 06:01:18 +0000

    Modifying 1 decimal digits to 2 on "Payroll By Instructor", "Pay Details By Instructor", and the "Billing Information By Client" reports/screens.

--- trunk/NewVersionProjectScheduler/frmReportBillingInfo.designer.cs
+++ trunk/NewVersionProjectScheduler/frmReportBillingInfo.designer.cs
@@ -124,19 +124,19 @@ namespace Scheduler
             this.printPreviewToolStripMenuItem,
             this.exportToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(186, 94);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(164, 76);
             // 
             // printToolStripMenuItem
             // 
             this.printToolStripMenuItem.Name = "printToolStripMenuItem";
-            this.printToolStripMenuItem.Size = new System.Drawing.Size(185, 30);
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(163, 24);
             this.printToolStripMenuItem.Text = "Print";
             this.printToolStripMenuItem.Click += new System.EventHandler(this.printToolStripMenuItem_Click);
             // 
             // printPreviewToolStripMenuItem
             // 
             this.printPreviewToolStripMenuItem.Name = "printPreviewToolStripMenuItem";
-            this.printPreviewToolStripMenuItem.Size = new System.Drawing.Size(185, 30);
+            this.printPreviewToolStripMenuItem.Size = new System.Drawing.Size(163, 24);
             this.printPreviewToolStripMenuItem.Text = "Print Preview";
             this.printPreviewToolStripMenuItem.Click += new System.EventHandler(this.printPreviewToolStripMenuItem_Click);
             // 
@@ -148,35 +148,35 @@ namespace Scheduler
             this.exportToXMLToolStripMenuItem,
             this.exportToTXTToolStripMenuItem});
             this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
-            this.exportToolStripMenuItem.Size = new System.Drawing.Size(185, 30);
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(163, 24);
             this.exportToolStripMenuItem.Text = "Export";
             this.exportToolStripMenuItem.Click += new System.EventHandler(this.exportToolStripMenuItem_Click);
             // 
             // exportToolStripMenuItem1
             // 
             this.exportToolStripMenuItem1.Name = "exportToolStripMenuItem1";
-            this.exportToolStripMenuItem1.Size = new System.Drawing.Size(208, 30);
+            this.exportToolStripMenuItem1.Size = new System.Drawing.Size(182, 24);
             this.exportToolStripMenuItem1.Text = "Export To Excel";
             this.exportToolStripMenuItem1.Click += new System.EventHandler(this.exportToolStripMenuItem1_Click);
             // 
             // exportToHTMLToolStripMenuItem
             // 
             this.exportToHTMLToolStripMenuItem.Name = "exportToHTMLToolStripMenuItem";
-            this.exportToHTMLToolStripMenuItem.Size = new System.Drawing.Size(208, 30);
+            this.exportToHTMLToolStripMenuItem.Size = new System.Drawing.Size(182, 24);
             this.exportToHTMLToolStripMenuItem.Text = "Export to HTML";
             this.exportToHTMLToolStripMenuItem.Click += new System.EventHandler(this.exportToHTMLToolStripMenuItem_Click);
             // 
             // exportToXMLToolStripMenuItem
             // 
             this.exportToXMLToolStripMenuItem.Name = "exportToXMLToolStripMenuItem";
-            this.exportToXMLToolStripMenuItem.Size = new System.Drawing.Size(208, 30);
+            this.exportToXMLToolStripMenuItem.Size = new System.Drawing.Size(182, 24);
             this.exportToXMLToolStripMenuItem.Text = "Export to XML";
             this.exportToXMLToolStripMenuItem.Click += new System.EventHandler(this.exportToXMLToolStripMenuItem_Click);
             // 
             // exportToTXTToolStripMenuItem
             // 
             this.exportToTXTToolStripMenuItem.Name = "exportToTXTToolStripMenuItem";
-            this.exportToTXTToolStripMenuItem.Size = new System.Drawing.Size(208, 30);
+            this.exportToTXTToolStripMenuItem.Size = new System.Drawing.Size(182, 24);
             this.exportToTXTToolStripMenuItem.Text = "Export to TXT";
             this.exportToTXTToolStripMenuItem.Click += new System.EventHandler(this.exportToTXTToolStripMenuItem_Click);
             // 
@@ -355,7 +355,7 @@ namespace Scheduler
             // 
             this.colPaidHours.Caption = "Paid Hours";
             this.colPaidHours.ColumnEdit = this.repositoryItemSpinEdit2;
-            this.colPaidHours.DisplayFormat.FormatString = "{0:n1}";
+            this.colPaidHours.DisplayFormat.FormatString = "{0:n2}";
             this.colPaidHours.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric;
             this.colPaidHours.FieldName = "PaidHours";
             this.colPaidHours.GroupFormat.FormatString = "{0:n1}";
@@ -403,7 +403,7 @@ namespace Scheduler
             // 
             this.colScheduledHours.Caption = "Scheduled Hours";
             this.colScheduledHours.ColumnEdit = this.repositoryItemSpinEdit1;
-            this.colScheduledHours.DisplayFormat.FormatString = "{0:n1}";
+            this.colScheduledHours.DisplayFormat.FormatString = "{0:n2}";
             this.colScheduledHours.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric;
             this.colScheduledHours.FieldName = "ScheduledHours";
             this.colScheduledHours.GroupFormat.FormatString = "{0:n1}";
@@ -517,7 +517,7 @@ namespace Scheduler
             this.checkEdit2.Location = new System.Drawing.Point(23, 49);
             this.checkEdit2.Name = "checkEdit2";
             this.checkEdit2.Properties.Caption = "End Date:";
-            this.checkEdit2.Size = new System.Drawing.Size(75, 24);
+            this.checkEdit2.Size = new System.Drawing.Size(75, 21);
             this.checkEdit2.TabIndex = 12;
             this.checkEdit2.CheckedChanged += new System.EventHandler(this.checkEdit2_CheckedChanged);
             // 
@@ -526,7 +526,7 @@ namespace Scheduler
             this.checkEdit1.Location = new System.Drawing.Point(23, 24);
             this.checkEdit1.Name = "checkEdit1";
             this.checkEdit1.Properties.Caption = "Start Date:";
-            this.checkEdit1.Size = new System.Drawing.Size(75, 24);
+            this.checkEdit1.Size = new System.Drawing.Size(75, 21);
             this.checkEdit1.TabIndex = 11;
             this.checkEdit1.CheckedChanged += new System.EventHandler(this.checkEdit1_CheckedChanged);
             // 
@@ -551,7 +551,7 @@ namespace Scheduler
             this.dateEditEndDate.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime;
             this.dateEditEndDate.Properties.VistaTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton()});
-            this.dateEditEndDate.Size = new System.Drawing.Size(169, 26);
+            this.dateEditEndDate.Size = new System.Drawing.Size(169, 22);
             this.dateEditEndDate.TabIndex = 9;
             // 
             // dateEditStartDate
@@ -566,7 +566,7 @@ namespace Scheduler
             this.dateEditStartDate.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime;
             this.dateEditStartDate.Properties.VistaTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton()});
-            this.dateEditStartDate.Size = new System.Drawing.Size(169, 26);
+            this.dateEditStartDate.Size = new System.Drawing.Size(169, 22);
             this.dateEditStartDate.TabIndex = 8;
             // 
             // frmReportBillingInfo
